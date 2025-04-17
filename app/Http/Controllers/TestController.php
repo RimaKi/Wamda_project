b09eba0318a1d4a91861e4be79dc6909fda500ef
OlaHarb1 <olaharb524@gmail.com>
2025-04-17 22:33:35 +0300

    send email to parent

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FilterRequest;
+use App\Http\Requests\SendEmailRequest;
 use App\Http\Resources\QuestionResource;
 use App\Http\Resources\TestResource;
+use App\Mail\SendEmailToParentRequest;
 use App\Mail\SentNotificationEmail;
 use App\Models\Child;
 use App\Models\Group;
@@ -33,6 +35,19 @@ class TestController extends Controller
     {
         Mail::to(User::query()->where("role", "expert")->first()->email)->send(new SentNotificationEmail([
             'name' => $child->name,
+        ]));
+        return self::success();
+    }
+    public function sendEmailToParent(SendEmailRequest $request)
+    {
+        $data=$request->validated();
+        $parent=User::query()->where("id", $data["parentId"])->firstorFail();
+        if(!$parent->children()->where('_id', $data["childId"])->exists()){
+            throw new \Exception("there are wrong parent email");
+        }
+        Mail::to($parent->email)->send(new SendEmailToParentRequest([
+            'childName' => Child::where("_id", $data["childId"])->first()->name,
+            "parent" => $parent,
         ]));
         return self::success();
     }
---
+++ resources/views/emailToParent.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>امتحان جديد</title>
+</head>
+<body>
+
+<div class="message">
+    <p style="font-size: medium "> عزيزي <span>{{$data["parent"]["firstName"]}}</span> </p>
+
+    <p>تم الإنتهاء من تصحيح اختبار الطفل </p>
+    <p>{{$data["childName"]}}</p>
+</div>
+
+</body>
+<html>
