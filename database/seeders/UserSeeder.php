e188652babb9b93ca2ca34ce9e7c125e124c1de1
RimaKi <rimakiwan1999@gmail.com>
2025-04-29 12:46:51 +0300

    modified seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,10 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Testing\Fluent\Concerns\Has;
 
 class UserSeeder extends Seeder
 {
@@ -16,18 +14,18 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-       User::create([
-                "firstName"=>"Amal",
-                "lastName"=>"Al halabi",
-                "email"=>"amal2017abbas@gmail.com",
-                "password"=>Hash::make("123123"),
-                "isMale"=>1,
-                "summary"=>"Consultant in speech pathology and learning difficulties at the Institute of Hearing and Speech",
-            "certification"=>"A speech and language pathologist",
-            "address"=>"Riyadh",
-            "phone"=>"0996655444",
-            "role"=>"expert"
-            ]);
+        User::create([
+            "firstName" => "Amal",
+            "lastName" => "Al halabi",
+            "email" => "amal2017abbas@gmail.com",
+            "password" => Hash::make("amal@1234"),
+            "isMale" => 1,
+            "summary" => "Consultant in speech pathology and learning difficulties at the Institute of Hearing and Speech",
+            "certification" => "A speech and language pathologist",
+            "address" => "Riyadh",
+            "phone" => "0996655444",
+            "role" => "expert"
+        ]);
 
     }
 }
